0613b63318ad7dc11cf981603999b62ce32b8965
“dsoper2” <dsoper@cisco.com>
2021-05-20 09:59:22 -0500

    k8s provider with client and cluster credentials from variables

--- k8s/main.tf
+++ k8s/main.tf
@@ -8,7 +8,11 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path = var.config_path
+  # config_path = var.config_path
+  host                   = var.host
+  client_certificate     = base64decode(var.client_cert)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_cert)
 }
 
 resource "kubernetes_deployment" "webapp" {

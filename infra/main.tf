34dcb291442378bede9d118d09d7382464b72956
“dsoper2” <dsoper@cisco.com>
2021-05-20 17:26:57 -0500

    change to 1 server for k8s demo

--- infra/main.tf
+++ infra/main.tf
@@ -9,6 +9,7 @@ module "imm_profile" {
 
   # Server names and platform
   server_list = [
+    /*
     {
       name                   = "SJC07-R14-FI-1-1-4",
       object_type            = "compute.Blade",
@@ -23,6 +24,7 @@ module "imm_profile" {
       vmedia_policy          = "tf-esxi67u3-248-231",
       boot_iso_file_location = "172.28.224.72/kubam/imm-esx-172-22-248-231.iso"
     },
+    */
     {
       name                   = "SJC07-R14-FI-1-1-6",
       object_type            = "compute.Blade",
@@ -36,15 +38,15 @@ module "imm_profile" {
   server_profile_action = var.server_profile_action
 
   # Comment out any policies you do not want configured
-  imc_access_policy = "tf-module-SJC07-R14-15-access"
+  imc_access_policy = "tf-k8s-SJC07-R14-15-access"
   imc_access_vlan   = 248
   ip_pool           = "SJC07-R14-15-IP"
 
-  boot_order_policy = "tf-module-boot-policy"
+  boot_order_policy = "tf-k8s-boot-policy"
 
-  lan_connectivity_policy = "tf-module-lan-connectivity-policy"
+  lan_connectivity_policy = "tf-k8s-lan-connectivity-policy"
   mac_pool                = "sjc02-de30-mac"
-  ethernet_network_group  = "tf-module-sjc07-248-net-group"
+  ethernet_network_group  = "tf-k8s-sjc07-248-net-group"
   cluster_vlan            = 248
   vnic_name               = var.vnic_name
 }
